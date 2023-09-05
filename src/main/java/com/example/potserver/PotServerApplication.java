0388e75e7343f708da617a37622d1a8557f7cb10
user <b57062562@gmail.com>
2023-09-05 12:32:37 +0900

    🌱 :: first commit

---
+++ src/main/java/com/example/potserver/PotServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.potserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PotServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/potserver/PotServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.potserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
