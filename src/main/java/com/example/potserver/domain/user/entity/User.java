3fa6a6e19f28e392a1bf99208ec54890ae0bf74f
user <b57062562@gmail.com>
2023-09-05 16:51:59 +0900

    🌧 :: user entity 추가

---
+++ src/main/java/com/example/potserver/domain/user/entity/User.java
@@ -0,0 +1,25 @@
+package com.example.potserver.domain.user.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userId;
+
+    private String password;
+
+}
