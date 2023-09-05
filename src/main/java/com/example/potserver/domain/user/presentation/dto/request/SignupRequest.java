c5d6a20cde88dd15249d212b4fc25734e592ebbd
user <b57062562@gmail.com>
2023-09-05 17:02:09 +0900

    🌧 :: 회원가입 dto 추가

---
+++ src/main/java/com/example/potserver/domain/user/presentation/dto/request/SignupRequest.java
@@ -0,0 +1,14 @@
+package com.example.potserver.domain.user.presentation.dto.request;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SignupRequest {
+
+    private String userId;
+
+    private String password;
+}
