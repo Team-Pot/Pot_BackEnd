7512116b57cfc32379c9b18fd99e76cb874612ac
user <b57062562@gmail.com>
2023-09-09 18:58:29 +0900

    🌧 :: refreshToken 레포지토리 수정

--- src/main/java/com/example/potserver/domain/auth/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/potserver/domain/auth/repository/RefreshTokenRepository.java
@@ -1,7 +1,7 @@
 package com.example.potserver.domain.auth.repository;
 
 import com.example.potserver.domain.auth.repository.entity.RefreshToken;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
 }
