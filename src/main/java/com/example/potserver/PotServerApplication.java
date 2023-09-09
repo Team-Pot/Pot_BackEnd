28957bcec1874aa1efdeb2a7d260780e7a2c4fd1
user <b57062562@gmail.com>
2023-09-09 18:58:04 +0900

    🐛:: apllication 클래스 어노테이션 추가

--- src/main/java/com/example/potserver/PotServerApplication.java
+++ src/main/java/com/example/potserver/PotServerApplication.java
@@ -2,8 +2,10 @@ package com.example.potserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class PotServerApplication {
 
     public static void main(String[] args) {
