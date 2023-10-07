95203e2a725c018211a4664fcd30d7ad2a039813
user <b57062562@gmail.com>
2023-10-07 18:52:16 +0900

    🪓 :: 사용하지 않는 exception 삭제

--- src/main/java/com/example/potserver/global/error/exception/ErrorCode.java
+++ src/main/java/com/example/potserver/global/error/exception/ErrorCode.java
@@ -16,15 +16,6 @@ public enum ErrorCode {
     //user
     USER_NOT_FOUND(404, "User not found"),
     PASSWORD_MISMATCH(403, "Password mismatch"),
-    ALREADY_COMMENT(409, "Already write comment"),
-    ALREADY_LIKE(409, "Already like"),
-    ALREADY_DISLIKE(409, "Already dis_like"),
-
-    //work
-    WORK_NOT_FOUND(404, "Work not found"),
-    LIKE_NOT_FOUND(404, "Like not found"),
-    DIS_LIKE_NOT_FOUND(404, "DisLike not found"),
-    COMMENT_NOT_FOUND(404, "Comment not found"),
 
     // general
     BAD_REQUEST(400, "Bad request"),
