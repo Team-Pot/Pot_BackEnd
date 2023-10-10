9bcd9a5bcab28fc541dd84b85e3dda5c6db56162
user <b57062562@gmail.com>
2023-10-10 12:35:13 +0900

    🌧 :: memberService 추가

---
+++ src/main/java/com/example/potserver/domain/user/service/MemberService.java
@@ -0,0 +1,78 @@
+package com.example.potserver.domain.user.service;
+
+import com.example.potserver.domain.user.entity.User;
+import com.example.potserver.domain.user.presentation.dto.response.EmailVerificationResult;
+import com.example.potserver.domain.user.repository.UserRepository;
+import com.example.potserver.global.exception.user.UnableSuchAlgorithmException;
+import com.example.potserver.global.exception.user.UserExistsException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.time.Duration;
+import java.util.Optional;
+import java.util.Random;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+
+    private static final String AUTH_CODE_PREFIX = "AuthCode ";
+
+    private final UserRepository memberRepository;
+
+    private final MailService mailService;
+
+    private final RedisService redisService;
+
+    @Value("${spring.mail.auth-code-expiration-millis}")
+    private long authCodeExpirationMillis;
+
+	public void sendCodeToEmail(String toEmail) {
+        this.checkDuplicatedEmail(toEmail);
+        String title = "Travel with me 이메일 인증 번호";
+        String authCode = this.createCode();
+        mailService.sendEmail(toEmail, title, authCode);
+        // 이메일 인증 요청 시 인증 번호 Redis에 저장 ( key = "AuthCode " + Email / value = AuthCode )
+        redisService.setValues(AUTH_CODE_PREFIX + toEmail,
+                authCode, Duration.ofMillis(this.authCodeExpirationMillis));
+    }
+
+    private void checkDuplicatedEmail(String email) {
+        Optional<User> member = memberRepository.findByEmail(email);
+        if (member.isPresent()) {
+            log.debug("MemberServiceImpl.checkDuplicatedEmail exception occur email: {}", email);
+            throw UserExistsException.EXCEPTION;
+        }
+    }
+
+    private String createCode() {
+        int lenth = 6;
+        try {
+            Random random = SecureRandom.getInstanceStrong();
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < lenth; i++) {
+                builder.append(random.nextInt(10));
+            }
+            return builder.toString();
+        } catch (NoSuchAlgorithmException e) {
+            log.debug("MemberService.createCode() exception occur");
+            throw UnableSuchAlgorithmException.EXCEPTION;
+        }
+    }
+
+    public EmailVerificationResult verifiedCode(String email, String authCode) {
+        this.checkDuplicatedEmail(email);
+        String redisAuthCode = redisService.getValues(AUTH_CODE_PREFIX + email);
+        boolean authResult = redisService.checkExistsValue(redisAuthCode) && redisAuthCode.equals(authCode);
+
+        return EmailVerificationResult.of(authResult);
+    }
+
+}
