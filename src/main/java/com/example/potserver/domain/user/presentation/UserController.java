f4c1810caab39899b085f1cc7dfca2494936a614
user <b57062562@gmail.com>
2023-10-24 21:09:35 +0900

    ♻️ :: 리펙토링

--- src/main/java/com/example/potserver/domain/user/presentation/UserController.java
+++ src/main/java/com/example/potserver/domain/user/presentation/UserController.java
@@ -2,14 +2,10 @@ package com.example.potserver.domain.user.presentation;
 
 import com.example.potserver.domain.user.presentation.dto.request.CheckEmailRequest;
 import com.example.potserver.domain.user.presentation.dto.request.SendEmailRequest;
-import com.example.potserver.domain.user.presentation.dto.response.EmailVerificationResult;
-import com.example.potserver.domain.user.presentation.dto.response.SingleResponseDto;
 import com.example.potserver.domain.user.service.CheckCodeService;
 import com.example.potserver.domain.user.service.SendCodeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
